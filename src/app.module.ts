661b9ad2c00dbb9daaa6d9d12cb719656c44e024
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-07-15 15:16:01 +0100

    adding_env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService,  } from '@nestjs/config';
 import { User } from './user/entities/user.entity';
 
 
@@ -12,16 +12,30 @@ import { User } from './user/entities/user.entity';
     ConfigModule.forRoot({
       isGlobal : true
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      // host: 'dpg-cq98u1jv2p9s73cfhb8g-a',
-      host: 'dpg-cq98u1jv2p9s73cfhb8g-a',
-      port: 5432,
-      username: 'muky_albany',
-      password: 'iOoWAcFOvQUPnqooXU9II2kvNLgd7KQ0',
-      database: 'grambutler',
-      entities: [User],
-      synchronize: true,
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   // host: 'dpg-cq98u1jv2p9s73cfhb8g-a',
+    //   host: 'dpg-cq98u1jv2p9s73cfhb8g-a',
+    //   port: 5432,
+    //   username: 'muky_albany',
+    //   password: 'iOoWAcFOvQUPnqooXU9II2kvNLgd7KQ0',
+    //   database: 'grambutler',
+    //   entities: [User],
+    //   synchronize: true,
+    // }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USERNAME'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [User],
+        synchronize: true,
+      }),
     }),
     UserModule,
   ],
