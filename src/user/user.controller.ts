ebfc138721817aed8ca6b9d5b44b09c1beba29aa
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-07-15 14:09:52 +0100

    adding_login

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -78,5 +78,70 @@ export class UserController {
     }
   }
 
+  @Post('login')
+  async login(@Body() createAuthDto: CreateAuthDto, @Res({ passthrough: true }) response: Response): Promise<any> {
+    console.log(createAuthDto.email)
+    console.log(createAuthDto.password)
+  try {      
+    
+    const result = await this.userService.login(createAuthDto);
+    console.log(result)
+    const  email =  result.email
+    const  id =  result.id
+    const  role =  result.role
+    const payload = { email: email, sub: id };
+    const rolePayload = { role: role, sub: id };
+
+    // Sign JWT for access token with a longer expiry time
+    const jwtTokenKeys = await this.jwt.signAsync(payload, {
+      expiresIn: '35s',
+      secret: this.configService.get<string>('ACCESS_TOKEN'),   
+    });
+
+    // Sign JWT for refresh token with a longer expiry time
+    const jwtRefreshTokenKeys = await this.jwt.signAsync(payload, {
+      expiresIn: '7d',
+      secret: this.configService.get<string>('REFRESH_TOKEN'),   
+    });
+
+      // Sign JWT for role token with a longer expiry time
+      const roleToken = await this.jwt.signAsync(rolePayload, {
+        expiresIn: '7d',
+        secret: this.configService.get<string>('ROLE_TOKEN'),   
+      });
+
+      // Set HttpOnly cookie for the access token
+    response.cookie('accessToken', jwtTokenKeys, {
+      // httpOnly: false,
+      // secure: false,
+      maxAge: 7 * 60 * 60 * 1000,  // 7 hours in milliseconds
+      // path: '/',
+      // sameSite: 'none',
+    });
+
+    // Set HttpOnly cookie for the refresh token (if needed)
+    response.cookie('refreshToken', jwtRefreshTokenKeys, {
+      // httpOnly: false,
+      // secure: false,
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
+      // path: '/', 
+      // sameSite: 'none',
+    });
+
+    // Set HttpOnly cookie for the role token (if needed)
+    response.cookie('roleToken', roleToken, {
+      // httpOnly: true,
+      // secure: true,
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
+      // path: '/', 
+      // sameSite: 'none',
+    });
+return result;
+  } catch (error) {
+    console.error('User creation failed', error);
+    throw error;
+  }
+}
+
 
 }
