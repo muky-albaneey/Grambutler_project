0e5ccc31e73178264c27be168da70a1d6aaf0418
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-07-15 07:00:52 +0100

    adding_create_account_api

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,7 @@ import { User } from './user/entities/user.entity';
     TypeOrmModule.forRoot({
       type: 'postgres',
       // host: 'dpg-cq98u1jv2p9s73cfhb8g-a',
-      host: 'dpg-cq98u1jv2p9s73cfhb8g-a' || 'db',
+      host: 'dpg-cq98u1jv2p9s73cfhb8g-a',
       port: 5432,
       username: 'muky_albany',
       password: 'iOoWAcFOvQUPnqooXU9II2kvNLgd7KQ0',
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,13 +1,37 @@
+// import { Module } from '@nestjs/common';
+// import { UserService } from './user.service';
+// import { UserController } from './user.controller';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { User } from './entities/user.entity';
+// import { JwtService } from '@nestjs/jwt';
+
+// @Module({
+//   imports: [TypeOrmModule.forFeature([User])],
+//   controllers: [UserController],
+//   providers: [UserService, JwtService],
+// })
+// export class UserModule {}
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { JwtService } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('ACCESS_TOKEN'),
+        signOptions: { expiresIn: '60s' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [UserController],
-  providers: [UserService, JwtService],
+  providers: [UserService],
 })
 export class UserModule {}
