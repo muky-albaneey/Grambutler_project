59d029c33cb00d033760bc00c401c2b3a6842d43
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-07-15 15:53:01 +0100

    adding_env

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,17 +12,6 @@ import { User } from './user/entities/user.entity';
     ConfigModule.forRoot({
       isGlobal : true
     }),
-    // TypeOrmModule.forRoot({
-    //   type: 'postgres',
-    //   // host: 'dpg-cq98u1jv2p9s73cfhb8g-a',
-    //   host: 'dpg-cq98u1jv2p9s73cfhb8g-a',
-    //   port: 5432,
-    //   username: 'muky_albany',
-    //   password: 'iOoWAcFOvQUPnqooXU9II2kvNLgd7KQ0',
-    //   database: 'grambutler',
-    //   entities: [User],
-    //   synchronize: true,
-    // }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
