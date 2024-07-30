600f1b251a02c5052cdb91238d62a1055623716e
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-07-30 06:42:34 +0100

    adding settinggs

--- src/user/entities/setting.entity.ts
+++ src/user/entities/setting.entity.ts
@@ -9,16 +9,16 @@ export class Settings{
     @Column({nullable : true, length: 190, type: 'varchar'})
     firstname?: string;
 
-    @Column({ type: 'varchar', length: 190, nullable: false })
+    @Column({ type: 'varchar', length: 190, nullable: true })
     lastname: string;
 
-    @Column({ type: 'varchar', length: 190, nullable: false  })
+    @Column({ type: 'varchar', length: 190, nullable: true  })
     email: string;
 
-    @Column({ type: 'varchar', length: 190, nullable: false  })
+    @Column({ type: 'varchar', length: 190, nullable: true  })
     username : string;
 
-    @Column({ type: 'varchar', length: 190, nullable: false  })
+    @Column({ type: 'varchar', length: 190, nullable: true  })
     location: string;
 
     
