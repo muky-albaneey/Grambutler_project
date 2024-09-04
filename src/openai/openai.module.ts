f7479d7a8ef1b5ed55e3de38109c3ae8fdc3b4c3
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-09-04 09:22:06 +0100

    attachhing the AI to thhe user

--- src/openai/openai.module.ts
+++ src/openai/openai.module.ts
@@ -7,6 +7,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [ TypeOrmModule.forFeature([ResponseEntity]),HttpModule],
+  exports: [TypeOrmModule],
   providers: [OpenaiService],
   controllers: [OpenaiController]
 })
