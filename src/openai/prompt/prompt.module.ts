930332033b946a5f59f3219f13e19b7b63c77e0d
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-09-07 06:06:10 +0100

    attaching the AI to the user

--- src/openai/prompt/prompt.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PromptService } from './prompt.service';
-import { PromptController } from './prompt.controller';
-
-@Module({
-  providers: [PromptService],
-  controllers: [PromptController]
-})
-export class PromptModule {}
