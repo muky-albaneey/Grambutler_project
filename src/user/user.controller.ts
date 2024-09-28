fc583ca7203638644cf967d988d84c9154fbb77d
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-09-28 17:09:13 +0100

    addding response code

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -188,84 +188,150 @@ async reset(@Body() body: { token: string }) {
   }
 
   @Get('all')
-  async findAll() {
-      return await this.userService.findAll();
+  async findAll( @Res({ passthrough: true }) response: Response) {
+      const result = await this.userService.findAll();
+      return response.status(HttpStatus.OK).json({
+        statusCode: HttpStatus.OK,
+        message: 'All Users',
+        data: result,
+      
+      });
     }
 
   @Get(':id/single_user')
-  async findOne(@Param('id', ParseUUIDPipe) id: string) {
-      return await this.userService.findOne(id);
+  async findOne(@Param('id', ParseUUIDPipe) id: string,  @Res({ passthrough: true }) response: Response) {
+      const result = await this.userService.findOne(id);
+      return response.status(HttpStatus.OK).json({
+        statusCode: HttpStatus.OK,
+        message: 'Single User',
+        data: result,
+      
+      });
     }
 
   @Patch(':id/onboard')
-  async onoardingScreen(@Param('id', ParseUUIDPipe) id: string, @Body() body : OnboardingDto){
-    return await this.userService.updateOnboarding(id, body)
+  async onoardingScreen(@Param('id', ParseUUIDPipe) id: string, @Body() body : OnboardingDto, @Res({ passthrough: true }) response: Response){
+    const result= await this.userService.updateOnboarding(id, body)
+    return response.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      message: 'Onboarding info',
+      data: result,
+    
+    });
   }
 
   @Patch(':id/profileImg')
   @UseInterceptors(FileInterceptor ('profile'))
   @UsePipes(new ValidationPipe({ transform: true }))
-  async uploadProfile(@Param('id', ParseUUIDPipe) id: string, @UploadedFile() file: Express.Multer.File) {
+  async uploadProfile(@Param('id', ParseUUIDPipe) id: string, @UploadedFile() file: Express.Multer.File, @Res({ passthrough: true }) response: Response) {
   // async createProfileImg(@Param('id', ParseUUIDPipe) id: string, @Body() createFileDto) {  
     
     const result = await this.userService.updateProfileBg(id, file); 
     console.log(result)
-    return result;
+    return response.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      message: 'All Users',
+      data: result,
+    
+    });
   }
 
   @Patch(':id/settings')
-  async userSetting(@Param('id', ParseUUIDPipe) id: string, @Body() body : SettingDto){
+  async userSetting(@Param('id', ParseUUIDPipe) id: string, @Body() body : SettingDto, @Res({ passthrough: true }) response: Response){
     return await this.userService.updateSetting(id, body)
   }
 
   // In your UserController
 
 @Post(':userId/follow/:targetUserId')
-async followUser(@Param('userId', ParseUUIDPipe) userId: string, @Param('targetUserId', ParseUUIDPipe) targetUserId: string) {
+async followUser(@Param('userId', ParseUUIDPipe) userId: string, @Param('targetUserId', ParseUUIDPipe) targetUserId: string, @Res({ passthrough: true }) response: Response) {
   console.log(userId, targetUserId  )
   await this.userService.followUser(userId, targetUserId);
-  return { message: 'Successfully followed the user' };
+  // return { message: 'Successfully followed the user' };
+  return response.status(HttpStatus.OK).json({
+    statusCode: HttpStatus.OK,
+    message: 'Successfully followed the user',
+    // data: result,
+  
+  });
 }
 
 
 @Post(':userId/unfollow/:targetUserId')
 async unfollowUser(
   @Param('userId', ParseUUIDPipe) userId: string, 
-  @Param('targetUserId', ParseUUIDPipe) targetUserId: string
+  @Param('targetUserId', ParseUUIDPipe) targetUserId: string,
+  @Res({ passthrough: true }) response: Response
 ) {
   console.log('Unfollow Request - userId:', userId, 'targetUserId:', targetUserId);
   await this.userService.unfollowUser(userId, targetUserId);
-  return { message: 'Successfully unfollowed the user' };
+  // return { message: 'Successfully unfollowed the user' };
+  return response.status(HttpStatus.OK).json({
+    statusCode: HttpStatus.OK,
+    message: 'Successfully unfollowed the user',
+    // data: result,
+  
+  });
 }
 
 
 @Get(':userId/followers')
-async getFollowers(@Param('userId',ParseUUIDPipe) userId: string) {
+async getFollowers(@Param('userId',ParseUUIDPipe) userId: string, @Res({ passthrough: true }) response: Response) {
   const followers = await this.userService.getFollowers(userId);
-  return followers;
+  // return followers;
+  return response.status(HttpStatus.OK).json({
+    statusCode: HttpStatus.OK,
+    message: 'list of followers',
+    data: followers,
+  
+  });
 }
 
 @Get(':userId/following')
-async getFollowing(@Param('userId',ParseUUIDPipe) userId: string) {
+async getFollowing(@Param('userId',ParseUUIDPipe) userId: string, @Res({ passthrough: true }) response: Response) {
   const following = await this.userService.getFollowing(userId);
-  return following;
+  // return following;
+  return response.status(HttpStatus.OK).json({
+    statusCode: HttpStatus.OK,
+    message: 'list of following',
+    data: following,
+  
+  });
 }
 
 @Get(':userId/followers/count')
-async countFollowers(@Param('userId', ParseUUIDPipe) userId: string): Promise<number> {
-  return await this.userService.countFollowers(userId);
+async countFollowers(@Param('userId', ParseUUIDPipe) userId: string, @Res({ passthrough: true }) response: Response) {
+  const result = await this.userService.countFollowers(userId);
+  return response.status(HttpStatus.OK).json({
+    statusCode: HttpStatus.OK,
+    message: 'number of followers',
+    data: result,
+  
+  });
 }
 
   // In your UserController
   @Get(':userId/following/count')
-  async countFollowing(@Param('userId', ParseUUIDPipe) userId: string): Promise<number> {
-    return await this.userService.countFollowing(userId);
+  async countFollowing(@Param('userId', ParseUUIDPipe) userId: string, @Res({ passthrough: true }) response: Response) {
+    const result = await this.userService.countFollowing(userId);
+    return response.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      message: 'number of user u are following',
+      data: result,
+    
+    });
   }
 
     // Get all posts from users that the current user follows
   @Get('/followed/:userId')
-  async getPostsFromFollowedUsers(@Param('userId') userId: string) {
-    return await this.userService.getPostsFromFollowedUsers(userId);
+  async getPostsFromFollowedUsers(@Param('userId') userId: string, @Res({ passthrough: true }) response: Response) {
+    const result = await this.userService.getPostsFromFollowedUsers(userId);
+    return response.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      message: 'Get all posts from users that the current user follows',
+      data: result,
+    
+    });
   }
 
   @Post(':userId/post_create')
@@ -274,8 +340,16 @@ async createPostWithImage(
   @Param('userId') userId: string,
   @Body() createPostDto: CreatePostDto,
   @UploadedFile() file?: Express.Multer.File, // Image is optional
+  // @Res({ passthrough: true }) response: Response
 ) {
-  return await this.userService.createPostWithImage(createPostDto, userId, file);
+  const result = await this.userService.createPostWithImage(createPostDto, userId, file);
+return result;
+    //  return response.status(HttpStatus.OK).json({
+    //   statusCode: HttpStatus.OK,
+    //   message: 'Get all posts from users that the current user follows',
+    //   data: result,
+    
+    // });
 }
 
   // Add a comment to a post
@@ -283,9 +357,16 @@ async createPostWithImage(
 async addComment(
   @Param('postId', ParseUUIDPipe) postId: string,
   @Body('userId') userId: string,
-  @Body('content') content: string
+  @Body('content') content: string,
+  @Res({ passthrough: true }) response: Response
 ) {
-  return await this.userService.addComment(postId, userId, content);
+  const result = await this.userService.addComment(postId, userId, content);
+  return response.status(HttpStatus.OK).json({
+    statusCode: HttpStatus.OK,
+    message: 'Add a comment to a post',
+    data: result,
+  
+  });
 }
 
 
@@ -293,30 +374,61 @@ async addComment(
   @Post(':postId/like')
   async likePost(
     @Param('postId') postId: string,
-    @Body('userId') userId: string
+    @Body('userId') userId: string,
+    @Res({ passthrough: true }) response: Response
   ) {
-    return await this.userService.likePost(postId, userId);
+    const result= await this.userService.likePost(postId, userId);
+    return response.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      message: 'Like a post',
+      data: result,
+    
+    });
   }
 
 @Get('all_posts')
-async getAllPostsWithCategory() {
-  return await this.userService.getAllPostsWithCategory();
+async getAllPostsWithCategory(@Res({ passthrough: true }) response: Response) {
+  const result=  await this.userService.getAllPostsWithCategory();
+   return response.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      message: ' all post',
+      data: result,
+    
+    });
 }
 
 @Get('posts/count-by-user')
-async getPostCountByUser(): Promise<any> {
-  return await this.userService.countPostsByUser();
+async getPostCountByUser(@Res({ passthrough: true }) response: Response): Promise<any> {
+  const result = await this.userService.countPostsByUser();
+   return response.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      message: 'number of  a post of a user',
+      data: result,
+    
+    });
 }
 
 @Get(':userId/post-count')
-async countPostsByUser(@Param('userId') userId: string): Promise<any> {
-  return await this.userService.countPostsByUserHimself(userId);
+async countPostsByUser(@Param('userId') userId: string, @Res({ passthrough: true }) response: Response): Promise<any> {
+  const result = await this.userService.countPostsByUserHimself(userId);
+   return response.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      message: 'number of  a post of a user',
+      data: result,
+    
+    });
 }
 
 @Get(':userId/posts-with-likes')
-async getPostsWithLikesByUser(@Param('userId', ParseUUIDPipe) userId: string) {
+async getPostsWithLikesByUser(@Param('userId', ParseUUIDPipe) userId: string, @Res({ passthrough: true }) response: Response) {
   const posts = await this.userService.countPostsWithLikesByUser(userId);
-  return posts;
+   return response.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      message: 'Like a post',
+      data: posts,
+    
+    });
+  // return posts;
 }
 
 
