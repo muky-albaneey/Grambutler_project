0c5f84b4163b19ee5baa3d87f5d21e1e4fde4e45
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-12-11 15:41:22 +0100

    making change to the AI analytic

--- src/stripe/stripe.module.ts
+++ src/stripe/stripe.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StripeController } from './stripe.controller';
 import { StripeService } from './stripe.service';
+import { PaymentService } from 'src/payment/payment.service';
 
 @Module({
   controllers: [StripeController],
-  providers: [StripeService],
+  providers: [PaymentService,StripeService],
 })
 export class StripeModule {}
