eb8d51d0d15f6397e67f6a86ff7d0b65c5cf67ad
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-12-18 17:16:08 +0100

    s3 bucket update

--- src/user/entities/profile.entity.ts
+++ src/user/entities/profile.entity.ts
@@ -1,11 +1,11 @@
 /* eslint-disable prettier/prettier */
 
-import { OneToOne, JoinColumn, OneToMany, ManyToOne, Entity } from 'typeorm';
+import { OneToOne, JoinColumn, OneToMany, Column, Entity } from 'typeorm';
 import { AbstractFileEntity } from './abstract.entity';
 
 
 @Entity()
 export class ProfileImage extends AbstractFileEntity<ProfileImage> {
-    @Column({ type: 'varchar', nullable: true })
+  @Column({ type: 'varchar', nullable: true })
   url: string; // URL for S3 bucket
 }
