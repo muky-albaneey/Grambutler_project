1e6a20cd60a0be7719d31d1f6b9538086fe81f32
abdulwahab abbas <abdulwahababbas300@gmail.com>
2025-01-12 16:52:01 +0100

    Changing the url of the success url so that we can confirm payment again

--- src/stripe/stripe.service.ts
+++ src/stripe/stripe.service.ts
@@ -41,7 +41,7 @@
         },
       ],
       mode: 'payment',
-      success_url: "https://grambutler.vercel.app/payment-success/sessionId={CHECKOUT_SESSION_ID}",
+      success_url: "https://grambutler.vercel.app/payment-success/{CHECKOUT_SESSION_ID}",
       // success_url: "https://grambutler-project-d8bz.onrender.com/stripe/session?session_id={CHECKOUT_SESSION_ID}",
       cancel_url: "https://grambutler.vercel.app/pricing", // Add your failure URL here
     });
