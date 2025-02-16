f9447c2fb60d50d5871ae0d66b8ed00a277570b6
abdulwahab abbas <abdulwahababbas300@gmail.com>
2025-02-16 23:43:17 +0100

    correcting the task color correction

--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -54,7 +54,7 @@ import { Comment_task } from './comment.entity';
     taskStar: string;
 
     @Column({ type: 'varchar', length: 50 })
-    TaskColor: string;
+    taskColor: string;
   
     @Column({ type: 'int', nullable: true })
     startReminder: number;
